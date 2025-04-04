cbbcdfd89af8d747452e3f9389fb3f161034906e
Amodhx <amxdhnanditha@gmail.com>
2025-04-04 20:38:54 +0530

    created login and defined routes

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    MatFormField,
+    MatLabel,
+    MatFormField,
+    MatInput,
+    MatButton
+  ],
   templateUrl: './login.component.html',
+  standalone: true,
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  constructor(private router: Router) {
+  }
+  onSignup() {
+    this.router.navigate(['/dashboard']); // Redirect after signup
+  }
 }
