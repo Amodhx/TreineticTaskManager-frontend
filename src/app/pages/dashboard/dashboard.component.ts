6223822de798d5ea86e1e909e3b406af979bb136
Amodhx <amxdhnanditha@gmail.com>
2025-04-04 22:36:54 +0530

    created add button and filter combo

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
-
+import {SidebarComponent} from "../../sidebar/sidebar.component";
+import {MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
+import {MatDivider} from "@angular/material/divider";
+import {MatButton} from "@angular/material/button";
+import {MatButtonToggle, MatButtonToggleGroup} from "@angular/material/button-toggle";
+import {MatGridList, MatGridTile} from "@angular/material/grid-list";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatChip} from "@angular/material/chips";
+import {DatePipe, NgForOf} from "@angular/common";
+interface Task {
+  id: number;
+  title: string;
+  description: string;
+  status: string;
+  createdAt: string;
+}
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatDivider,
+    MatButton,
+    MatButtonToggleGroup,
+    MatButtonToggle,
+    MatGridList,
+    MatGridTile,
+    MatCard,
+    MatCardContent,
+    MatPaginator,
+    MatChip,
+    MatCardHeader,
+    DatePipe,
+    MatCardSubtitle,
+    MatCardTitle,
+    NgForOf
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
 
 }
+
+
