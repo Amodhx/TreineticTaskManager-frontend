af4705a1da12b95b7d65ce5551266495971ff978
Amodhx <amxdhnanditha@gmail.com>
2025-04-04 23:10:37 +0530

    added task table

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,20 +1,34 @@
 import { Component } from '@angular/core';
-import {SidebarComponent} from "../../sidebar/sidebar.component";
-import {MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
-import {MatDivider} from "@angular/material/divider";
-import {MatButton} from "@angular/material/button";
-import {MatButtonToggle, MatButtonToggleGroup} from "@angular/material/button-toggle";
-import {MatGridList, MatGridTile} from "@angular/material/grid-list";
-import {MatPaginator} from "@angular/material/paginator";
-import {MatChip} from "@angular/material/chips";
-import {DatePipe, NgForOf} from "@angular/common";
-interface Task {
-  id: number;
-  title: string;
-  description: string;
-  status: string;
-  createdAt: string;
+import {MatDivider} from '@angular/material/divider';
+import {MatButton} from '@angular/material/button';
+import {MatButtonToggle, MatButtonToggleGroup} from '@angular/material/button-toggle';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef,
+  MatTable
+} from '@angular/material/table';
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
 }
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -23,23 +37,29 @@ interface Task {
     MatButton,
     MatButtonToggleGroup,
     MatButtonToggle,
-    MatGridList,
-    MatGridTile,
-    MatCard,
-    MatCardContent,
-    MatPaginator,
-    MatChip,
-    MatCardHeader,
-    DatePipe,
-    MatCardSubtitle,
-    MatCardTitle,
-    NgForOf
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatHeaderCellDef,
+    MatCell,
+    MatCellDef,
+    MatHeaderRow,
+    MatRow,
+    MatRowDef,
+    MatHeaderRowDef
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
 
+  onRowClicked(inde:any){
+    console.log(inde)
+  }
+
+  protected readonly indexedDB = indexedDB;
 }
 
 
